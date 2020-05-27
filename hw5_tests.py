f23937e080e39059a56e9e00d80cc338b00fe98a
Itamar Shiryan <itamar.shiryan@gmail.com>
2020-05-27 16:44:02 +0300

    Added binary tree + fixed a test in permutations

--- hw5_tests.py
+++ hw5_tests.py
@@ -7,25 +7,29 @@ def checker_equals(func, expected):
     return func == expected
 
 
-def test(cases, func, max_time=None, checker_func=checker_equals):
+def test_case(args, expected_result, func, max_time=None, checker_func=checker_equals):
+    func_pretty_name = f"{func.__name__}({', '.join(repr(i) for i in args)})"
+
+    start = time.perf_counter()
+    try:
+        result = func(*args)
+    except Exception:
+        print(f"{func_pretty_name}: an error has occured during function\n", traceback.format_exc())
+        return
+
+    end = time.perf_counter()
+    if not checker_func(result, expected_result):
+        print(f"{func_pretty_name}: expected {expected_result}, got {result}")
+
+    if max_time is not None:
+        elapsed_time = end - start
+        if elapsed_time > max_time:
+            print(f"{func_pretty_name}: took {elapsed_time} seconds")
+
+
+def test_cases(cases, func, max_time=None, checker_func=checker_equals):
     for args, expected_result in cases:
-        func_pretty_name = f"{func.__name__}({', '.join(repr(i) for i in args)})"
-
-        start = time.perf_counter()
-        try:
-            result = func(*args)
-        except Exception:
-            print(f"{func_pretty_name}: an error has occured during function\n", traceback.format_exc())
-            continue
-
-        end = time.perf_counter()
-        if not checker_func(result, expected_result):
-            print(f"{func_pretty_name}: expected {expected_result}, got {result}")
-
-        if max_time is not None:
-            elapsed_time = end - start
-            if elapsed_time > max_time:
-                print(f"{func_pretty_name}: took {elapsed_time} seconds")
+        test_case(args, expected_result, func, max_time, checker_func)
 
 
 def test_perm():
@@ -43,7 +47,7 @@ def test_perm():
         perm_obj = hw.Permutation(perm)
         return perm_obj.perm
 
-    test(cases, test_perm_validation)
+    test_cases(cases, test_perm_validation)
 
     # __getitem__
     cases = [
@@ -56,7 +60,7 @@ def test_perm():
         perm_obj = hw.Permutation(perm)
         return perm_obj[i]
 
-    test(cases, test_perm_getitem)
+    test_cases(cases, test_perm_getitem)
 
     # compose
     cases = [
@@ -68,7 +72,7 @@ def test_perm():
         perm_obj2 = hw.Permutation(p2)
         return perm_obj1.compose(perm_obj2).perm
 
-    test(cases, test_perm_compose)
+    test_cases(cases, test_perm_compose)
 
     # inv
     cases = [
@@ -79,7 +83,7 @@ def test_perm():
         perm_obj = hw.Permutation(perm)
         return perm_obj.inv().perm
 
-    test(cases, test_perm_inv)
+    test_cases(cases, test_perm_inv)
 
     # compose_list
     cases = [
@@ -90,24 +94,69 @@ def test_perm():
         lst = [hw.Permutation(perm) for perm in perms]
         return hw.compose_list(lst).perm
 
-    test(cases, test_perm_compose)
+    test_cases(cases, test_perm_compose)
 
     # order
     cases = [
         (([0, 1, 2, 3],), 1),
         (([1, 0, 2, 3],), 2),
-        (([2, 3, 1, 0],), 3)
+        (([2, 3, 1, 0],), 4)
     ]
 
     def test_perm_order(perm):
         return hw.Permutation(perm).order()
 
-    test(cases, test_perm_order)
+    test_cases(cases, test_perm_order)
+
+
+def test_binary_tree():#
+    def build_tree(tree_items):
+        tree_obj = hw.Binary_search_tree()
+        for key, val in tree_items:
+            assert type(key) is str
+            assert type(val) in (float, int)
+            tree_obj.insert(key, val)
+        return tree_obj
+
+    # max_sum
+    cases = [
+        (([],), 0),
+        (([('e', 1), ('b', 2)],), 3),
+        (([('e', 1), ('b', 2), ('a', 8), ('d', 4), ('c', 10), ('i', 3), ('g', 5)
+              , ('f', 7), ('h', 9), ('j', 6), ('k', 5)],), 18),
+    ]
+
+    def test_max_sum(tree_items):
+        return build_tree(tree_items).max_sum()
+    test_cases(cases, test_max_sum)
+
+    # is_balanced
+    cases = [
+        (([],), True),
+        (([("b", 10), ("d", 10), ("a", 10), ("c", 10)],), True),
+        (([("b", 10), ("d", 10), ("a", 10), ("c", 10), ("e", 10), ("f", 10)],), False),
+    ]
+
+    def test_is_balanced(tree_items):
+        return build_tree(tree_items).is_balanced()
+    test_cases(cases, test_is_balanced)
+
+    # diam
+    cases = [
+        (([],), 0),
+        (([('c', 10), ('a', 10), ('b', 10), ('g', 10), ('e', 10), ('d', 10), ('f', 10), ('h', 10)],), 6),
+        (([('c', 1), ('g', 3), ('e', 5), ('d', 7), ('f', 8), ('h', 6), ('z', 6)],), 5),
+    ]
+
+    def test_diam(tree_items):
+        return build_tree(tree_items).diam()
+    test_cases(cases, test_diam)
 
 
 def run_all_tests():
     all_tests = [
-        test_perm
+        test_perm,
+        test_binary_tree
     ]
 
     for test_func in all_tests:
